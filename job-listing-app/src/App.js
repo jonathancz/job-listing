09d36c7c8c9d79ffe7a7d48365660d1a975673ab
Jonathan Chen <jonathanchenzheng@hotmail.com>
2020-10-20 13:39:59 -0400

    Study data and create Job Board Component

--- job-listing-app/src/App.js
+++ job-listing-app/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import JobBoardComponent from './components/JobComponentBoard';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <JobBoardComponent />
     </div>
   );
 }
 
 export default App;
+
+
+// TODO
+// 1. Study the design and data/API
+// 2. Create Job Board component (receive the data)
+// 3. Get the data from JSON 
+// 4. Pass down data to the Job Board component
+// 5. Styling
+// 6. Filter component
+// 7. Filter the data
---
+++ job-listing-app/src/components/JobComponentBoard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const JobBoardComponent = () => <div>Hi from the Job Board Component!</div>
+
+export default JobBoardComponent;
