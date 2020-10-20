841ee4b7f922e01e33235b22dc202b1817540b61
Jonathan Chen <jonathanchenzheng@hotmail.com>
2020-10-20 13:58:51 -0400

    Fetching data from Job Board Component

--- job-listing-app/src/App.js
+++ job-listing-app/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import JobBoardComponent from './components/JobComponentBoard';
+import React, { useState, useEffect } from 'react'
+import data from './service/data.json'
+import JobBoardComponent from './components/JobComponentBoard'
 
 function App() {
+  // Initialize as empty array
+  const [jobs, setJobs] = useState([])
+
+  useEffect(() => {
+    setJobs(data)
+  }, [])
+
+  console.log(data)
   return (
     <div className="App">
       <h1>Hello world!</h1>
-      <JobBoardComponent />
+      {
+        jobs.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+            jobs.map(job => (
+              <JobBoardComponent job={job} key={job.id} />
+            ))
+          )
+      }
+
     </div>
   );
 }
@@ -14,8 +32,8 @@ export default App;
 
 
 // TODO
-// 1. Study the design and data/API
-// 2. Create Job Board component (receive the data)
+// 1. Study the design and data/API *
+// 2. Create Job Board component (receive the data) *
 // 3. Get the data from JSON 
 // 4. Pass down data to the Job Board component
 // 5. Styling
