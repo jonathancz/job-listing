008b1f857656c5e3863b8f73d18082aa88b05692
Jonathan Chen <jonathanchenzheng@hotmail.com>
2020-10-21 12:07:22 -0400

    Stylize each job component and display relevant information

--- job-listing-app/src/components/JobComponentBoard.js
+++ job-listing-app/src/components/JobComponentBoard.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-const JobBoardComponent = ({job}) => (
-<div className="flex">
-    <div>
-        <img src={job.logo} alt={job.company} />
-    </div>
-    <div></div>
-    <div></div>
-</div>
-)
+const JobBoardComponent = ({ job }) => {
+    const langAndTools = []
+    if(job.languages) {
+        langAndTools.push(...job.languages)
+    }
+    if(job.tools) {
+        langAndTools.push(...job.tools)
+    }
+    return (
+        <div className="flex bg-white shadow-md m-4 p-4">
+            <div>
+                <img src={job.logo} alt={job.company} />
+            </div>
+
+            <div className="flex flex-col justify-between ml-4">
+                <h3 className="font-bold text-xl text-teal-500">{job.company}</h3>
+                <h2 className="font-bold text-xl">{job.position}</h2>
+                <p className="text-gray-700">
+                    {job.postedAt} · {job.contract} · {job.location}
+                </p>
+            </div>
+
+            <div className='flex items-center justify-center'>
+                {langAndTools ? langAndTools.map((langAndTool) =>
+                    <span className='text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded'>{langAndTool}</span>
+                ) : ''}
+            </div>
+        </div>
+    )
+}
 
 export default JobBoardComponent;
