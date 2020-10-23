63a53f2483f4eef100dbd3ab9bca05fc6a309b10
Jonathan Chen <jonathanchenzheng@hotmail.com>
2020-10-22 20:46:24 -0400

    Align elements within each job component

--- job-listing-app/src/components/JobComponentBoard.js
+++ job-listing-app/src/components/JobComponentBoard.js
@@ -9,20 +9,19 @@ const JobBoardComponent = ({ job }) => {
         langAndTools.push(...job.tools)
     }
     return (
-        <div className="flex bg-white shadow-md m-4 p-4">
+        <div className="flex bg-white shadow-md m-4 p-6">
             <div>
                 <img src={job.logo} alt={job.company} />
             </div>
-
             <div className="flex flex-col justify-between ml-4">
-                <h3 className="font-bold text-xl text-teal-500">{job.company}</h3>
+                <h3 className="font-bold text-teal-500">{job.company}</h3>
                 <h2 className="font-bold text-xl">{job.position}</h2>
                 <p className="text-gray-700">
                     {job.postedAt} · {job.contract} · {job.location}
                 </p>
             </div>
 
-            <div className='flex items-center justify-center'>
+            <div className='flex items-center justify-end ml-auto'>
                 {langAndTools ? langAndTools.map((langAndTool) =>
                     <span className='text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded'>{langAndTool}</span>
                 ) : ''}
