a89bc5bb9719cdbcb6984fc629fa223927186cde
Jonathan Chen <jonathanchenzheng@hotmail.com>
2020-10-23 21:45:10 -0400

    Add conditional CSS rendering if job listing is New or Featured

--- job-listing-app/src/components/JobComponentBoard.js
+++ job-listing-app/src/components/JobComponentBoard.js
@@ -26,15 +26,15 @@ const JobBoardComponent = ({
     }
 
     return (
-        <div className="flex bg-white shadow-md m-4 p-6 rounded">
+        <div className={`flex bg-white shadow-md m-4 p-6 rounded ${featured && 'border-l-8 border-teal-500 border-solid'}`}>
             <div>
                 <img src={logo} alt={company} />
             </div>
             <div className="flex flex-col justify-between ml-4">
                 <h3 className="font-bold text-teal-500">
                     {company}
-                    {isNew && <span className='text-teal-500 bg-teal-100 font-bold p-2 rounded'>New</span>}
-                    {featured && <span className='text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded'>Featured</span>}
+                    {isNew && <span className='bg-teal-500 text-teal-100 font-bold m-1 py-1 px-2 rounded-full'>New</span>}
+                    {featured && <span className='bg-gray-800 text-white font-bold m-1 py-1 px-2 rounded-full'>Featured</span>}
                 </h3>
                 <h2 className="font-bold text-xl">{position}</h2>
                 <p className="text-gray-700">
